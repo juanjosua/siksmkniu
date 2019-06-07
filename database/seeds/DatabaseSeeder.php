e4c6e303a086b1a2fe519ac988af91c8a6415c7d
Juan Josua <juan_josua@apps.ipb.ac.id>
2019-06-07 23:43:32 +0700

    database seeder work

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Pegawai;
+use App\Staf;
+use App\Pimpinan;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,10 +16,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        //seeder admin
         DB::table('admins')->insert([
             'username' => 'admin',
             'password' => bcrypt('admin')
         ]);
+
+        //seeder staf baru
+        $staf = new Staf();
+        $staf->save();
+        $pegawai =  new Pegawai();
+        $pegawai->nama_pegawai = 'Donna';
+        $pegawai->nip = '3275052410960014';
+        $pegawai->email_pegawai = 'donna@gmail.com';
+        $pegawai->no_telp_pegawai = '0821128747595';
+        $pegawai->password = bcrypt('secret');
+        $pegawai->jabatanable()->associate($staf);
+        $pegawai->save();
+
+        //seeder pimpinan baru
+        $pimpinan = new Pimpinan();
+        $pimpinan->save();
+        $pegawai =  new Pegawai();
+        $pegawai->nama_pegawai = 'Danu';
+        $pegawai->nip = '3275051010970014';
+        $pegawai->email_pegawai = 'danu@gmail.com';
+        $pegawai->no_telp_pegawai = '0821128740987';
+        $pegawai->password = bcrypt('secret');
+        $pegawai->jabatanable()->associate($pimpinan);
+        $pegawai->save();
+
     }
 }
